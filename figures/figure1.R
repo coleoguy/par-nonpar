b7998a669f06b4a998c7e164e5b169104cfc4255
Carl Hjelmen <chjelmen@bio.tamu.edu>
2022-01-27 15:25:00 -0600

    here

--- figures/figure1.R
+++ figures/figure1.R
@@ -1,4 +1,5 @@
 library(stringr)
+results <- read.csv("../results/results.csv")
 results$h <- as.numeric(str_sub(results$h, start=5))
 results$r <- as.numeric(str_sub(results$r, start=5))
 results$s <- as.numeric(str_sub(results$s, start=5))
@@ -11,7 +12,7 @@ X <- data.frame(results[, c(1,3:6)])
 Y <- data.frame(results[, 2:6])
 colnames(X)[1] <- colnames(Y)[1] <- "Freq"
 both <- rbind(X, Y)
-both$chromosome <- rep(c("X","Y"), each = 36000)
+both$chromosome <- rep(c("X","Y"), each = 360000)
 
 # library(ggraptR)
 # ggraptR(both)
@@ -27,3 +28,13 @@ ggplot(R4, aes(y=Freq_median, x=s)) +
   theme(text=element_text(family="sans", face="plain", color="#000000", size=15, hjust=0.5, vjust=0.5)) + 
   scale_size(range=c(1, 3)) + xlab("s") + ylab("Freq_median")
 
+ggraptR(R1)
+
+
+
+
+foo <- R1[R1$s==.5,]
+foo <- foo[foo$h==.5,]
+foo <- foo[foo$model=="model = auto.and.nonparX",]
+hist(foo$Freq[foo$chromosome=="X"])
+hist(foo$Freq[foo$chromosome=="Y"])
